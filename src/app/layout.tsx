b435d127d74e050264c8fbb8b54bc7d13ca5534a
Vinay Sharma <vinaysharma@Vinays-MacBook-Air.local>
2025-02-17 12:36:02 +0530

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,8 +29,8 @@ const geistMono = localFont({ // Create a local font for Geist Mono
 // Define the metadata object
 export const metadata: Metadata = { // Define the metadata object
   title: { // Define the title object
-    template: "Modewelt | %s", // Define the title template
-    default: "Modewelt", // Define the default title
+    template: "Modeweltjob | %s", // Define the title template
+    default: "Modeweltjob", // Define the default title
   },
   description: "Connect to the world of fashion and style", // Define the description
   keywords: ["fashion", "style", "clothing", "apparel", "fashion careers", "fashion jobs",

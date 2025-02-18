1e8d132d1510b0da448c1ddfdc87a2bfb4f4fce6
Anuj3553 <anujverma3553@gmail.com>
2025-02-18 11:26:20 +0530

    Refactor JobSection and MyJobPost components for user-specific job listings and improved error handling

--- src/components/Jobs/MyJobSection/MyJobPost.jsx
+++ src/components/Jobs/MyJobSection/MyJobPost.jsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Image from "next/image";
 import { useRouter } from 'next/navigation';
-import Bg from "../../../../public/assets/jobmenu/image.png";
 import { formatRelativeDate } from "@/lib/utils";
 
 export default function MyJobPost({ job }) {
@@ -14,7 +12,7 @@ export default function MyJobPost({ job }) {
     const jobStatus = expirationDate && expirationDate > currentDate ? "Active" : "Expired";
     return (
         <div className="bg-white max-h-[80vh] overflow-y-auto hover:shadow-md border rounded-lg p-4 sm:p-6">
-            {job ? (
+            {job && (
                 <div
                     key={job.id}
                     className="flex items-center gap-4 p-4 border-b last:border-none cursor-pointer"
@@ -32,20 +30,6 @@ export default function MyJobPost({ job }) {
                     </div>
                     {/* <button className="text-gray-500 hover:text-gray-700">
                         <FaEllipsisH className="text-2xl" />
-                    </button> */}
-                </div>
-            ) : (
-                <div className="flex flex-col items-center justify-center w-full bg-white p-6 rounded-lg mb-6">
-                    <Image src={Bg} alt="No jobs found" />
-                    <h2 className="mt-4 text-lg font-semibold text-gray-700">
-                        No Recent Job Activity
-                    </h2>
-                    <p className="text-gray-500 text-center px-4">
-                        Find new opportunities and manage your job search progress here.
-                    </p>
-                    {/* Job Search */}
-                    {/* <button className="mt-4 bg-[#ba669d] text-white px-6 py-2 rounded-full hover:bg-[#9d4f80]">
-                        SEARCH JOB
                     </button> */}
                 </div>
             )}

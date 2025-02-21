f0ff5941f5bba9b0ffd218c20560b839becf8f77
vinay2003 <vinaysharma31681@gmail.com>
2025-02-21 17:10:29 +0530

    chore: update devDependencies for @types/node, @types/react, and typescript versions

--- src/app/admin/dashboard/page.jsx
+++ src/app/admin/dashboard/page.jsx
@@ -2,8 +2,8 @@
 import { useState } from "react";
 import PieChart from "@/app/ui/dashboard/dashboard/piechart";
 import GrowthChart from "@/app/ui/dashboard/dashboard/growthchart";
-import RevenueChart from "@/app/ui/dashboard/dashboard/revenuechart";
-import UserMapChart from "../ui/dashboard/dashboard/usermapchart";
+// import RevenueChart from "@/app/ui/dashboard/dashboard/revenuechart";
+// import UserMapChart from "../ui/dashboard/dashboard/usermapchart";
 import { FiCalendar } from "react-icons/fi";
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 

728e9b623540be1ba72cc045c606a567de9e0ec8
vinay2003 <vinaysharma31681@gmail.com>
2025-02-26 12:14:14 +0530

    refactor: update styling for button container in Post component

--- src/components/Feed/Post/index.tsx
+++ src/components/Feed/Post/index.tsx
@@ -141,7 +141,7 @@ export default function Post({ post }: PostProps) {
                   </button>
                 </Link>
               )}
-              <div>
+              <div className="flex items-center justify-center w-full py-1 px-4 text-sm rounded hover:text-red hover:font-semibold">
                 <button
                   onClick={() => setShowModal(true)}
                   className="block w-full text-left px-4 py-2 text-sm hover:bg-gray-100"

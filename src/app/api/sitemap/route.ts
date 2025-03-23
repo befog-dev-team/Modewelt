16cdc587cddac50b121d4f189fc437df51a17ca5
Anuj3553 <anujverma3553@gmail.com>
2025-03-23 11:06:45 +0530

    feat: add sitemap route and update robots.txt to include sitemap URL

---
+++ src/app/api/sitemap/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+
+export async function GET() {
+    const siteUrl = "https://modeweltjob.com";
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+        <url>
+            <loc>${siteUrl}/</loc>
+            <lastmod>${new Date().toISOString()}</lastmod>
+            <priority>1.0</priority>
+        </url>
+    </urlset>`;
+
+    return new NextResponse(sitemap, {
+        headers: {
+            "Content-Type": "application/xml",
+        },
+    });
+}
